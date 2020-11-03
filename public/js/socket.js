c0a7e4e2603ebcc24c834047b2dd71c3e14f0c19
JaeHyun Kim <carbitna03@gmail.com>
2020-11-04 00:08:25 +0900

    update

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,18 +1,32 @@
+const input = document.getElementById('m')
 const btn = document.getElementById('btn')
-const num = document.getElementById('num')
 
-var socket = io();
-        
 
-  $(function () {
-    var socket = io();
-    $('form').submit(function(e) {
-      e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
-  });
+
+const socket = io();
+socket.on('connect', () => {
+      var json = {
+      'name':'index',
+      'socketid' : socket.id
+  }
+      socket.emit('identify', json);
+});
+
+
+socket.on('chat message', (msg) => {
+    console.log(msg);
+    console.log(socket.id)
+});
+
+btn.addEventListener("click",()=>{
+  var json = {
+    'chat' : input.value,
+    'socketid' : socket.id
+  }
+  socket.emit('chat message', json);
+  
+  input.value=""
+})
 
 
 function upkey(){
