5f258ff80990b14a42818d9b962d633ce178205e
UniTaeMin <onnu0802@naver.com>
2020-11-04 08:54:44 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/app.js
+++ src/app.js
@@ -7,10 +7,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 var ip = require("ip");
 
-console.dir (ip.address());
+console.dir ( ip.address() );
 console.log(ip.address())
-
-//::1은 IpV6에서 로컬 호스트
 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
@@ -18,24 +16,22 @@ app.use('/assets',express.static(__dirname + '/assets'));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static("public"));
 
 app.get('/',function(req,res){
-	var ip = 
-	req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-	console.log(ip)
-
+	// var ip = 
+	// req.headers['x-forwarded-for'] ||
+    // req.connection.remoteAddress ||
+    // req.socket.remoteAddress ||
+    // req.connection.socket.remoteAddress;
+	// console.log(ip)
 	res.sendFile(__dirname+'/public/index.html');
 });
 
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(express.json({ limit: "30mb" }));
 app.set("view engine", "html");
-
 
 const dotenv = require("dotenv");
 const connectDB = require("./lib/connectDB");
@@ -48,44 +44,41 @@ connectDB();
 
 http.listen(port, () => console.log(`http://localhost:${port}`)).on("error", (err) => console.error(err));
 
-
 //나주엥 파일 분리 해주세요
 io.on("connection", (socket) => {
-	var mainroom = []
-	var fly=true
-	var roomescapeclear=true
+	var mainroom = [];
+	var fly = true;
+	var roomescapeclear = true;
 
 	console.log("a user connected");
 
 	socket.on("identify", (obj) => {
-		var id = obj.socketid
+		var id = obj.socketid;
 		//this.mainroom.push({id})
-		if(obj.name=="maingame"){
-			io.to(id).emit("data",{
-				"fly" : fly,
-				"roomescapeclear" : roomescapeclear
-			})
+		if (obj.name == "maingame") {
+			io.to(id).emit("data", {
+				fly: fly,
+				roomescapeclear: roomescapeclear,
+			});
 		}
 	});
 
-	socket.on("fly",()=>{
-		fly=true
-		socket.emit('fly')
-	})
+	socket.on("fly", () => {
+		fly = true;
+		socket.emit("fly");
+	});
 
-	socket.on("roomescapeclear",()=>{
-		roomescapeclear=true
-		console.log('r')
-		socket.emit('roomescapeclear')
-	})
+	socket.on("roomescapeclear", () => {
+		roomescapeclear = true;
+		console.log("r");
+		socket.emit("roomescapeclear");
+	});
 
 	//socket.on("disconnect", (data) => [userlist.deleteUser(socket.id)]);
-
 
 	console.log(socket.id);
 
 	socket.on("chat message", (json) => {
 		io.to(socket.id).emit("chat message", json.chat);
 	});
-
 });
